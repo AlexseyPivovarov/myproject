cb36b0f995dde32e906f011ba73101cb1de6ec26
AlexseyPivovarov <a.peevovarov@gmail.com>
2018-10-21 02:09:09 +0300

    pagination

--- core/views.py
+++ core/views.py
@@ -1,13 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import TemplateView
+from .models import Category, Product
+from django.core.paginator import Paginator
 
 
-# Create your views here.
-# def base(request):
-#     return HttpResponse('Hello world')
-class HomeView(TemplateView):
+# create your views here.
+def home(request):
+    context = {}
+    context['category'] = Category.objects.all()
+    # context['product'] = Product.objects.filter(product_vip=1).order_by('-product_datatime_creation').all()
+    paginator = Paginator(Product.objects.filter(product_vip=1).order_by('-product_datatime_creation').all(), 4)
+    context['product'] = paginator.get_page(request.GET.get('page'))
+    return render(request, template_name='core/home.html', context=context)
 
-    template_name = "base.html"
+
+def category_list(request, pid):
+    context = {}
+    context['category'] = Category.objects.all()
+    # context['product'] = Product.objects.filter(product_category__id=pid).order_by('-product_datatime_creation').all()
+    paginator = Paginator(Product.objects.filter(
+                product_category__id=pid).order_by('-product_datatime_creation').all(), 4)
+    context['product'] = paginator.get_page(request.GET.get('page'))
+    return render(request, template_name='core/home.html', context=context)
+
+
+def all_list(request):
+    context = {}
+    context['category'] = Category.objects.all()
+    # context['product'] = Product.objects.order_by('-product_datatime_creation').all()
+    paginator = Paginator(Product.objects.order_by('-product_datatime_creation').all(), 4)
+    context['product'] = paginator.get_page(request.GET.get('page'))
+    return render(request, template_name='core/home.html', context=context)
+
 
